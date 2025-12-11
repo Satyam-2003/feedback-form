9faf816a9d867f75db73b39c8ad538be7c8fb819
Satyam's Macbook <sunny@192.168.1.162>
2025-12-11 17:37:25 +0530

    add: Resolve bugs and intregare callbacks

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -50,13 +50,34 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
 
-  pages: {
-    signIn: 'sign-in'
-  },
-  
-  session: {
-    strategy: 'jwt'
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
   },
 
-  secret: process.env.NEXTAUTH_SECRET
+  pages: {
+    signIn: "sign-in",
+  },
+
+  session: {
+    strategy: "jwt",
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
 };
